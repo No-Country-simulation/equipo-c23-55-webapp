2da27fe3e07d85880596a94cc1236d57b33c6811
Patricio Barahona <p.barahonagallardo@gmail.com>
2025-01-22 17:14:44 -0300

    Implementación de formularios y vistas para autenticación de usuarios en 'users' y 'registration'

--- sandbox/apps/registration/urls.py
+++ sandbox/apps/registration/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'registration'
 urlpatterns = [
+    path('registration/', views.registration, name='registration'),
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('', include('django.contrib.auth.urls')),
 ]
--- sandbox/apps/registration/views.py
+++ sandbox/apps/registration/views.py
@@ -1,5 +1,28 @@
-from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
+
+from .forms import RegistrationForm
+
+
+def registration(request):
+    template = 'registration/registration.html'
+
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Has registrado tu cuenta con éxito.')
+            return redirect('core:home')
+        else:
+            messages.error(request, 'Las credenciales no cumplen con los requisitos o ya existen.')
+    else:
+        form = RegistrationForm()
+
+    context = {'form': form}
+    return render(request, template, context)
 
 
 class UserLoginView(LoginView):
